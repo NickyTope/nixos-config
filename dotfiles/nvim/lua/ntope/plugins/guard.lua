f8e340b4735cbad3974222575bf5bd9ee7a326f1
Nicky Tope <nicky.tope@gmail.com>
2024-10-14 09:11:57 +1100

    bring over updated nvim config

--- dotfiles/nvim/lua/ntope/plugins/guard.lua
+++ dotfiles/nvim/lua/ntope/plugins/guard.lua
@@ -13,9 +13,13 @@ return {
 		ft("scss"):fmt("lsp")
 		ft("css"):fmt("lsp")
 
-		require("guard").setup({
+		vim.g.guard_config = {
+			-- format on write to buffer
 			fmt_on_save = true,
-			lsp_as_default_fomatter = false,
-		})
+			-- use lsp if no formatter was defined for this filetype
+			lsp_as_default_formatter = false,
+			-- whether or not to save the buffer after formatting
+			save_on_fmt = true,
+		}
 	end,
 }
