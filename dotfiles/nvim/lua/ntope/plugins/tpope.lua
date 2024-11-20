a6e5cd3bf7fe83d86f93f8ec4e182330fb9fc8fc
Nicky Tope <nicky.tope@gmail.com>
2024-11-20 15:33:10 +1100

    - fugitive +neogit, openai

--- dotfiles/nvim/lua/ntope/plugins/tpope.lua
+++
@@ -1,8 +0,0 @@
-return {
-	{ "tpope/vim-fugitive", keys = {
-		{ "<leader>gs", "<cmd>G|18wincmd_<cr>", desc = "Git Status" },
-	} },
-	{ "tpope/vim-rhubarb", keys = {
-		{ "<leader>gh", "<cmd>GBrowse<cr>", desc = "Git Browse" },
-	} },
-}
