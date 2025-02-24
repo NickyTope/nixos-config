3bc1ed4029831cabe2b90db14da3fdc404d34cfd
Nicky Tope <nicky.tope@gmail.com>
2025-02-25 08:43:05 +1100

    updates, add second polybar for uwide

--- dotfiles/polybar/launch.sh
+++ dotfiles/polybar/launch.sh
@@ -2,15 +2,7 @@
 
 killall polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-# big=$(xrandr --listmonitors | grep 5120/ | wc -l)
-
-# for m in $(polybar --list-monitors | cut -d":" -f1); do
-#   if [ "$big" == "0" ]; then
-#     MONITOR=$m polybar -c ~/.config/polybar/config.ini --reload top &
-#     MONITOR=$m polybar -c ~/.config/polybar/config.ini --reload topright &
-#   else
-#     MONITOR=$m polybar -c ~/.config/polybar/config.ini --reload middle &
-#   fi
-# done
+big=$(xrandr --listmonitors | grep 5120/ | wc -l)
 
 polybar main -c $(dirname $0)/config.ini --reload &
+[ "$big" == "1" ] && polybar wide -c $(dirname $0)/config.ini --reload &
