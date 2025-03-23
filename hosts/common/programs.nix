6998083a73ac347b1b7bf344140c6475fc87ac4f
Nicky Tope <nicky.tope@gmail.com>
2025-03-24 08:54:44 +1100

    enable direnv

--- hosts/common/programs.nix
+++ hosts/common/programs.nix
@@ -8,6 +8,12 @@
 
   environment.pathsToLink = ["/share/zsh"];
 
+  programs.direnv = {
+    enable = true;
+    enableZshIntegration = true;
+    nix-direnv.enable = true;
+  };
+
   nix.settings = {
     # substituters = ["https://wezterm.cachix.org"];
     # trusted-public-keys = ["wezterm.cachix.org-1:kAbhjYUC9qvblTE+s7S+kl5XM1zVa4skO+E/1IDWdH0="];
