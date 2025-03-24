35427592a9557c885ec984855c2b4fbe0d1edd97
Nicky Tope <nicky.tope@gmail.com>
2025-03-24 15:22:39 +1100

    add dev shell flakes

---
+++ flake/terragrunt/flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Install pre-commit hooks";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          pre-commit
+          terragrunt
+          terramate
+          opentofu
+        ];
+      };
+    });
+}
